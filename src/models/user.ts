d1c49fe1beb45f9a7ab714b8b3d3960554aa6b4b
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-11 22:04:09 +0700

    fix: user schema declaration

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,44 +1,34 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: [
-    {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-  ],
-  email: [
-    {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-  ],
-  password: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  salt: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  role: [
-    {
-      type: String,
-      required: true,
-      enum: ["admin", "user"],
-      default: "user",
-    },
-  ],
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "user"],
+    default: "user",
+  },
 });
 
 export default mongoose.model("User", userSchema);
